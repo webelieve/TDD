8d79ac6c72823e7ac7f022da19a03ba52f8235b2
Fong <None>
2014-05-09 13:59:06 -0400

    Fixing negative number test case

--- wecode/src/main/java/com/we/code/tdd/stringcalculator/exception/NegativeNumberException.java
+++ wecode/src/main/java/com/we/code/tdd/stringcalculator/exception/NegativeNumberException.java
@@ -3,11 +3,9 @@ package com.we.code.tdd.stringcalculator.exception;
 public class NegativeNumberException extends RuntimeException {
 
 	private static final long serialVersionUID = 1167037705243822165L;
-	
-	private String errorMessage;
-	
+		
 	public NegativeNumberException(String errorMessage) {
-		this.errorMessage = errorMessage;
+		super(errorMessage);
 	}
 
 }
